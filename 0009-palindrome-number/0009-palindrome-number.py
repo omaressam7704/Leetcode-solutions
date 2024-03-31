80083b8f7bca43a08bf21ed03d59c52346a860fc
Omar Essam <64137057+omaressam7704@users.noreply.github.com>
2024-03-31 12:53:10 +0200

    Time: 54 ms (15.60%), Space: 11.5 MB (74.26%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        x1 = str(x)[::-1]
+        return int(x1) == x
