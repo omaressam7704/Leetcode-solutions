457ec30f8622465d828a91d4531952b52f672e5c
Omar Essam <64137057+omaressam7704@users.noreply.github.com>
2024-04-03 11:50:16 +0200

    Time: 1557 ms (32.50%), Space: 20.1 MB (81.15%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -0,0 +1,60 @@
+class Solution(object):
+    @staticmethod
+    # Implementation of MergeSort
+    def mergeSort(arr, s, e):
+        if e - s + 1 <= 1:
+            return arr
+
+        # The middle index of the array
+        m = (s + e) // 2
+
+        # Sort the left half
+        Solution.mergeSort(arr, s, m)
+
+        # Sort the right half
+        Solution.mergeSort(arr, m + 1, e)
+
+        # Merge sorted halves
+        Solution.merge(arr, s, m, e)
+    
+        return arr
+
+    @staticmethod
+    # Merge in-place
+    def merge(arr, s, m, e):
+        # Copy the sorted left & right halves to temp arrays
+        L = arr[s: m + 1]
+        R = arr[m + 1: e + 1]
+
+        i = 0  # index for L
+        j = 0  # index for R
+        k = s  # index for arr
+
+        # Merge the two sorted halves into the original array
+        while i < len(L) and j < len(R):
+            if L[i] <= R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        # One of the halves will have elements remaining
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+    def sortArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        if len(nums) == 1:
+            return nums
+        return self.mergeSort(nums, 0, len(nums) - 1)
